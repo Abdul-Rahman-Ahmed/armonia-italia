78f393e6ad061beb828eb684d8760109c7023cfe
abdulrahman <abdulrahman102034@gmail.com>
2025-06-11 11:52:40 +0200

    menu page

--- src/js/master.js
+++ src/js/master.js
@@ -3,49 +3,49 @@ const appetizers = [
     name: "بروشيتا بالطماطم والريحان",
     des: "خبز محمص مع زيتون وثوم وطماطم وزيت زيتون.",
     price: "80 ج.م",
-    src: "./imgs/dishes/Appetizers/Bruschetta.png",
+    src: "/imgs/dishes/Appetizers/Bruschetta.png",
   },
   {
     name: "أرانشيني (كرات الأرز المحشية)",
     des: "أرز، جبن موزاريلا، بقسماط، زيت للقلي",
     price: "90 ج.م",
-    src: "./imgs/dishes/Appetizers/Arancini.png",
+    src: "/imgs/dishes/Appetizers/Arancini.png",
   },
   {
     name: "كابوناتا",
     des: "خضروات مشوية مع صلصة الطماطم والثوم والبقدونس.",
     price: "65 ج.م",
-    src: "./imgs/dishes/Appetizers/Caponata.png",
+    src: "/imgs/dishes/Appetizers/Caponata.png",
   },
   {
     name: "أنتيباستو",
     des: "مجموعة من الخضروات المشوية مع صلصة البيستو.",
     price: "60 ج.م",
-    src: "./imgs/dishes/Appetizers/Antipasto.png",
+    src: "/imgs/dishes/Appetizers/Antipasto.png",
   },
   {
     name: "ميلانيزيانا",
     des: "شرائح الباذنجان المقلية مع صلصة الطماطم وجبنة الموتزاريلا.",
     price: "65 ج.م",
-    src: "./imgs/dishes/Appetizers/Melanzane.png",
+    src: "/imgs/dishes/Appetizers/Melanzane.png",
   },
   {
     name: "سالسيسا سيتيليانا",
     des: "سلطة الطماطم الطازجة مع البصل والزيتون والثوم والبقدونس.",
     price: "55 ج.م",
-    src: "./imgs/dishes/Appetizers/Salsiccia.png",
+    src: "/imgs/dishes/Appetizers/Salsiccia.png",
   },
   {
     name: "زوبا",
     des: "جمبري مشوي مع زيت الزيتون والثوم والليمون.",
     price: "80 ج.م",
-    src: "./imgs/dishes/Appetizers/Gamberi.png",
+    src: "/imgs/dishes/Appetizers/Gamberi.png",
   },
   {
     name: "تارتوفوليو",
     des: "بطاطس مقلية مع صلصة الطماطم والثوم والروزماري.",
     price: "70 ج.م",
-    src: "./imgs/dishes/Appetizers/Tartufoolio.png",
+    src: "/imgs/dishes/Appetizers/Tartufoolio.png",
   },
 ];
 
@@ -54,19 +54,19 @@ const main = [
     name: "فيتوتشيني ألفريدو",
     des: "مكرونة فيتوتشيني، كريمة طازجة، جبن بارميزان، زبدة، دجاج.",
     price: "180 ج.م",
-    src: "./imgs/dishes/Main/Fettuccine.png",
+    src: "/imgs/dishes/Main/Fettuccine.png",
   },
   {
     name: "ريزوتو بالفطر والبارميزان",
     des: "أرز أبوريو مطهو بمرق الدجاج مع فطر وجبن البارميزان.",
     price: "180 ج.م",
-    src: "./imgs/dishes/Main/Mushroom.png",
+    src: "/imgs/dishes/Main/Mushroom.png",
   },
   {
     name: "مانزو بيرا سيمبيتيكي",
     des: "مانزو بيرا سيمبيتيكي مع صلصة الجبنة والفلفل الأسود.",
     price: "225 ج.م",
-    src: "./imgs/dishes/Main/ManzoPerSebetti.png",
+    src: "/imgs/dishes/Main/ManzoPerSebetti.png",
   },
 ];
 
@@ -75,19 +75,19 @@ const pizza = [
     name: "بيتزا مارغريتا",
     des: "صلصة الطماطم، جبنة الموتزاريلا، زيت الزيتون، طماطم طازجة.",
     price: "150 ج.م",
-    src: "./imgs/dishes/Pizza/Margherita.png",
+    src: "/imgs/dishes/Pizza/Margherita.png",
   },
   {
     name: "بيتزا كواترو فورماجي",
     des: "صلصة الطماطم، جبنة الموتزاريلا، جبن بارميزان، جبن روكفور، جبن جريير.",
     price: "180 ج.م",
-    src: "./imgs/dishes/Pizza/Quat.png",
+    src: "/imgs/dishes/Pizza/Quat.png",
   },
   {
     name: "بيتزا كابريزيوسا",
     des: "صلصة الطماطم، جبنة الموتزاريلا، طماطم مقطعة، أوريغانو، زيت زيتون.",
     price: "185 ج.م",
-    src: "./imgs/dishes/Pizza/Capricciosa.png",
+    src: "/imgs/dishes/Pizza/Capricciosa.png",
   },
 ];
 
@@ -96,19 +96,19 @@ const deserts = [
     name: "تيراميسو",
     des: "بسكويت الليدي فينجر، قهوة، ميرنغ، كاكاو بودرة، جبنة ماسكاربون.",
     price: "120 ج.م",
-    src: "./imgs/dishes/Deserts/Tiramisu.png",
+    src: "/imgs/dishes/Deserts/Tiramisu.png",
   },
   {
     name: "كانولي",
     des: "حلوى الكانولي المقرمشة المحشوة بخليط الريكوتا والسكر والفستق المجروش.",
     price: "140 ج.م",
-    src: "./imgs/dishes/Deserts/SicilianCannolI.png",
+    src: "/imgs/dishes/Deserts/SicilianCannolI.png",
   },
   {
     name: "تورتا كابريز",
     des: "تورتة كابريز الشهيرة المحضرة بالشوكولاتة واللوز والبيض.",
     price: "160 ج.م",
-    src: "./imgs/dishes/Deserts/caprese.png",
+    src: "/imgs/dishes/Deserts/caprese.png",
   },
 ];
 
@@ -117,31 +117,34 @@ const drinks = [
     name: "قهوة إيطالية",
     des: "إسبريسو، كابتشينو، أو لاتيه.",
     price: "70 ج.م",
-    src: "./imgs/dishes/Drinks/Espresso.png",
+    src: "/imgs/dishes/Drinks/Espresso.png",
   },
   {
     name: "ليمونادة بالنعناع",
     des: "مكونات: عصير ليمون طازج، أوراق نعناع، سكر، ماء",
     price: "100 ج.م",
-    src: "./imgs/dishes/Drinks/Mint.png",
+    src: "/imgs/dishes/Drinks/Mint.png",
   },
   {
     name: "شاي الزنجبيل",
     des: "شاي الزنجبيل المنعش مع لمسة من الليمون.",
     price: "90 ج.م",
-    src: "./imgs/dishes/Drinks/GingerTea.png",
+    src: "/imgs/dishes/Drinks/GingerTea.png",
   },
 ];
 
 const show = (num, type) => {
   const container = document.querySelector(".menushow .cards");
   container.innerHTML = "";
+  let url = window.location.href;
+  let path;
+  url.includes("pages") ? (path = "..") : (path = ".");
   type.forEach((item, index) => {
     if (index >= num) return;
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-        <div class="img"><img src="${item.src}" alt="${item.name}"></div>
+        <div class="img"><img src="${path + item.src}" alt="${item.name}"></div>
         <div class="text">
           <h2>${item.name}</h2>
           <div class="des">${item.des}</div>
@@ -171,7 +174,6 @@ menunav.forEach((el) => {
     const newType = el.getAttribute("data-type");
     if (currentType === newType) return;
     currentType = newType;
-
     show(4, typesMap[newType]);
   });
 });
